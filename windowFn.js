35d3207d4b02b55a5e1518ba6047f56e3b967558
VV <v1rgul.42@gmail.com>
2020-04-02 19:55:29 +0200

    Clean Taylor & Tukey

--- windowFn.js
+++ windowFn.js
@@ -1,18 +1,27 @@
 
-function interpolate(arr, t){
-	if(t >= 1) return arr[arr.length-1]
 
-	arrT = t * (arr.length-1)
-	var i = Math.floor(arrT)
-	var localT = arrT - i
-	var r = (1-localT) * arr[i] + (localT) * arr[i+1]
-	// console.log("interpolate", "t:", t, "arr.length:", arr.length, "arrT:", arrT, "i:", i, "localT:", localT, "[",arr[i], ",", arr[i+1], "] ->", r)
-	return r
-}
 
-var taylor = require('taylorwin');
-function taylorWindowFn(n, nbar, sll){
-	var taylorWindowArr = taylor(n, nbar, sll)
+
+function genTaylor(options){
+	options = options || {}
+	options.interpolationSteps   = options.interpolationSteps   || 256
+	options.sidelobesNumber      = options.sidelobesNumber      ||   4 // How many sidelobes should be kept at attenuation
+	options.sidelobesAttenuation = options.sidelobesAttenuation ||  35 // dB, attenuation
+
+	let taylor = require('taylorwin')
+	let taylorWindowArr = taylor(options.interpolationSteps, options.sidelobesNumber, options.sidelobesAttenuation)
+
+	function interpolate(arr, t){
+		if(t >= 1) return arr[arr.length-1]
+
+		arrT = t * (arr.length-1)
+		let i = Math.floor(arrT)
+		let localT = arrT - i
+		let r = (1-localT) * arr[i] + (localT) * arr[i+1]
+		// console.log("interpolate", "t:", t, "arr.length:", arr.length, "arrT:", arrT, "i:", i, "localT:", localT, "[",arr[i], ",", arr[i+1], "] ->", r)
+		return r
+	}
+
 	return function(t){
 		return interpolate(taylorWindowArr, t)
 	}
@@ -28,18 +37,25 @@ function cosineWindowFn(p){
 	}
 }
 
-function tukeyWindowFn(alpha){ return function(t){
-	let r
-	if        (t<alpha/2) {
-		r = 0.5+0.5*Math.cos( Math.PI*( 2*t/alpha-1 ) )
-	} else if (t<(1-alpha/2)) {
-		r = 1
-	} else {
-		r =  0.5+0.5*Math.cos( Math.PI*( (2*(t-1))/alpha + 1 ) )
+function genTukey(options){
+	options = options || {}
+	options.alpha = options.alpha || .5
+	let integral = options.alpha * 0.5 + (1-options.alpha) * 1.0
+	let alpha_2 = options.alpha/2
+	return function(t){
+		let r
+		if        ( t<alpha_2     ) {
+			t = t/alpha_2
+			r = 0.5 + 0.5 * Math.cos( (t-1) * Math.PI )
+		} else if ( t<(1-alpha_2) ) {
+			r = 1
+		} else {
+			t = (t-1)/alpha_2
+			r = 0.5 + 0.5 * Math.cos( (t+1) * Math.PI )
+		}
+		return r / integral
 	}
-	return r * 4/3
-
-} }
+}
 
 
 
@@ -55,8 +71,8 @@ let windows = {
 	// BlackmanNuttal : cosineWindowFn([0.3635819, 0.4891775, 0.1365995, 0.0106411]),
 	// BlackmanHarris : cosineWindowFn([0.35875, 0.48829, 0.14128, 0.01168]),
 	// FlatTop        : cosineWindowFn([0.21557895, 0.41663158, 0.277263158, 0.083578947, 0.006947368]),
-	Taylor         : taylorWindowFn(256, 4, 35),
-	// Tukey          : tukeyWindowFn(0.5),
+	// Taylor         : genTaylor(),
+	Tukey          : genTukey(),
 }
 
 
